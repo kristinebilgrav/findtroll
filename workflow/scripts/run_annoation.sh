6a4f24a1898c238bb5235c575df58328925ba158
kristinebilgrav <77359122+kristinebilgrav@users.noreply.github.com>
2021-08-09 11:43:28 +0200

    Update run_annoation.sh

--- workflow/scripts/run_annoation.sh
+++ workflow/scripts/run_annoation.sh
@@ -6,9 +6,10 @@
 #SBATCH -J tabix_annotate
 
 module load bioinfo-tools tabix
+#python path/to/script path/to/input/file path/to/output/file/startname
 
-#python /proj/nobackup/sens2017106/kristine/SVDB/find_exons.py
-python /proj/nobackup/sens2017106/kristine/SVDB/find_tfs.py $1 /proj/nobackup/sens2017106/kristine/SVDB/$2/$2
+python /proj/nobackup/sens2017106/kristine/SVDB/find_exons.py $1 /proj/nobackup/sens2017106/kristine/SVDB/$2/$2
+python /proj/nobackup/sens2017106/kristine/SVDB/find_tfs.py /proj/nobackup/sens2017106/kristine/SVDB/$2/$2_VEP_exons.vcf /proj/nobackup/sens2017106/kristine/SVDB/$2/$2
 python /proj/nobackup/sens2017106/kristine/SVDB/find_segdups.py /proj/nobackup/sens2017106/kristine/SVDB/$2/$2_VEP_exons_tfs.vcf  /proj/nobackup/sens2017106/kristine/SVDB/$2/$2
 python /proj/nobackup/sens2017106/kristine/SVDB/find_DNase.py /proj/nobackup/sens2017106/kristine/SVDB/$2/$2_VEP_exons_tfs_segdup.vcf /proj/nobackup/sens2017106/kristine/SVDB/$2/$2
 
